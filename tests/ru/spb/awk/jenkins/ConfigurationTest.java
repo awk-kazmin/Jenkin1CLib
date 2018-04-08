ca7a604439f1eb16f44102757c1b5906d1a3dcc3
Василий Казьмин (awk) <awk795@gmail.com>
2018-04-08 23:04:24 +0300

    Init

---
+++ tests/ru/spb/awk/jenkins/ConfigurationTest.java
@@ -0,0 +1,38 @@
+package ru.spb.awk.jenkins;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConfigurationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void merge() {
+    }
+
+    @Test
+    public void imp() {
+    }
+
+    @Test
+    public void exp() {
+    }
+}

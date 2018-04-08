2a6860d2460b8af8cdb996a1fe826d19b789f38f
Василий Казьмин (awk) <awk795@gmail.com>
2018-04-08 22:43:40 +0300

    Init

---
+++ src/ru/spb/awk/jenkins/Configuration.groovy
@@ -0,0 +1,23 @@
+package ru.spb.awk.jenkins
+
+class Configuration implements Serializable {
+    def boolean load() {
+
+    }
+
+    def boolean save() {
+
+    }
+
+    def boolean merge() {
+
+    }
+
+    def boolean imp() {
+
+    }
+
+    def boolean exp() {
+
+    }
+}
